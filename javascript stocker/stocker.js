965db6b375d69b2d89b28b3c8f8249970615e971
vcorvid <vcorvid@gmail.com>
2018-01-22 11:48:20 -0800

    linting and start age calculator

--- javascript stocker/stocker.js
+++ javascript stocker/stocker.js
@@ -1,1 +1,8 @@
-var firstName = window.prompt("What's your first name?", "First Name");
+var firstName = prompt("What's your first name?");
+var lastName = prompt("What's your last name?");
+var age = prompt("How old are you?");
+alert("Hello " + firstName + " " + lastName + ", how are you?");
+alert("Only " + age + "? So young!")
+console.log(firstName + " " + lastName + " is awesome!");
+console.log("And " + age + " years old");
+
