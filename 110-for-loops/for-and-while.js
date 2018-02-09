237f996a6e8f225f28c1e22a40346f954a9aca93
vcorvid <vcorvid@gmail.com>
2018-02-08 22:36:29 -0800

    website, bulma, for and while

---
+++ 110-for-loops/for-and-while.js
@@ -0,0 +1,47 @@
+console.log("While: Print all numbers between -10 and 19");
+var num = -10;
+while (num <= 19){
+  console.log(num);
+  num++;
+}
+
+console.log("For: Print all numbers between -10 and 19")
+for (var i = -10; i <=19; i++){
+  console.log(i);
+}
+
+console.log("While: Print all even numbers between 10 and 40");
+var evenNum = 12;
+while (evenNum <= 40) {
+    console.log(evenNum);
+    evenNum +=2;
+}
+
+console.log("For: Print all even numbers between 10 and 40");
+for(var i =12; i < 40; i +=2){
+  console.log(i)
+}
+
+console.log("While: Print all odd numbers between 300 and 333");
+var oddNum = 301;
+while (oddNum<=333) {
+    console.log (oddNum);
+    oddNum+=2;
+}
+
+console.log("For: Print all odd numbers between 300 and 333");
+for(var i = 301; i < 333; i += 2){
+  console.log (i);
+}
+
+console.log("While: Print all numbers divisible by 5 and 3 between 5 and 50");
+var divis = 15;
+while (divis <= 50) {
+    console.log (divis);
+    divis += 15;
+}
+
+console.log("For: Print all numbers divisible by 5 and 3 between 5 and 50");
+for(var i = 15; i < 50; i += 15){
+  console.log (i);
+}
