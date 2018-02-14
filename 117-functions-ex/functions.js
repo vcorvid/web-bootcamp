54b2588ccbe661a2762e958c3b8923d1b7cf5028
vcorvid <vcorvid@gmail.com>
2018-02-13 16:09:32 -0800

    functions exercise

---
+++ 117-functions-ex/functions.js
@@ -0,0 +1,18 @@
+function isEven(x){
+  if (x % 2 == 0) {
+    return "Even"
+  } else {
+    return "Odd"
+  }
+}
+
+function factorial(z){
+  if (z == 0) {
+    return 1
+  } else {
+    var factorialResult = 1;
+    for (var i = z; i > 0 ; i --)
+    factorialResult * z;
+    return factorialResult;
+  }
+}
